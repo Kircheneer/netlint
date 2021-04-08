c3e628e92257e4eea4b2def11961139a4e631bec
Leo Kirchner <leo@kirchne.red>
2021-04-08 21:46:53 +0200

    Rework docs to include the README

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,6 @@ import sys
 
 sys.path.insert(0, os.path.abspath("../../"))
 
-
 # -- Project information -----------------------------------------------------
 
 project = "netlint"
@@ -25,13 +24,12 @@ author = "Leo Kirchner"
 # The full version, including alpha/beta/rc tags
 release = "0.1.0"
 
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx_rtd_theme", "sphinx.ext.autodoc"]
+extensions = ["sphinx_rtd_theme", "sphinx.ext.autodoc", "m2r2"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -40,7 +38,6 @@ templates_path = ["_templates"]
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
-
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -53,3 +50,5 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+source_suffix = ['.rst', '.md']
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,23 +1,4 @@
-Netlint Documentation
-=====================
-
-**Disclaimer: This is still in early development and therefore
-not very full-featured.**
-
-Lint static network device configuration file to identify any of
-the following problems:
-
-* configuration errors
-* security issues
-* ...
-
-Potential uses of this tool are
-
-* Linting network device configurations generated in
-  CI/CD automation pipelines
-* Assistance when building out new configurations for
-  both traditional and automated deployment
-* ...
+.. mdinclude:: ../../README.md
 
 Supported Network Operating Systems
 -----------------------------------
