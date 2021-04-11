f8a1a1292b4dadd9f23584abc76f1fce59bccc3a
Leo Kirchner <leo@kirchne.red>
2021-04-11 21:24:08 +0200

    add docstring to constants

--- netlint/checks/constants.py
+++ netlint/checks/constants.py
@@ -1,3 +1,6 @@
+"""Provide constants for checks across vendors.
+
+These might come from standards, but they don't have to."""
 bogus_as_numbers = [0, 23456, 4294967295]  # RFC6483, RFC7607  # RFC6793  # RFC7300
 bogus_as_numbers += list(range(64496, 64511 + 1))  # RFC5398
 bogus_as_numbers += list(range(65536, 65551 + 1))  # RFC4893, RFC5398
