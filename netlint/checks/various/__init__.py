d492868d6af9d11dba920a77e63406be6828bb4d
Leo Kirchner <leo@kirchne.red>
2021-04-13 21:15:37 +0200

    Add check for unused but configured access lists

--- netlint/checks/various/__init__.py
+++ netlint/checks/various/__init__.py
@@ -6,7 +6,7 @@ from ciscoconfparse import CiscoConfParse
 
 from netlint.checks.checker import CheckResult, Checker
 
-__all__ = ["check_default_snmp_communities"]
+__all__ = ["check_default_snmp_communities", "check_unused_access_lists"]
 
 from netlint.utils import NOS
 
@@ -27,3 +27,22 @@ def check_default_snmp_communities(
             )
 
     return None
+
+
+@Checker.register(apply_to=[NOS.CISCO_IOS, NOS.CISCO_NXOS], name="VAR102")
+def check_unused_access_lists(config: typing.List[str]) -> typing.Optional[CheckResult]:
+    """Check for any ACLs that are configured but never used."""
+    parsed_config = CiscoConfParse(config)
+    access_lists = parsed_config.find_lines(
+        r"^ip(v6)?\saccess-list\s(standard|extended)"
+    )
+    unused_acls = []
+    for acl in access_lists:
+        _, _, _, name = acl.split(" ", maxsplit=4)
+        usages = parsed_config.find_lines("(ip)? access-(group|class) {}".format(name))
+        if not usages:
+            unused_acls.append(acl)
+    if unused_acls:
+        return CheckResult(text="Unused ACLs configured", lines=unused_acls)
+    else:
+        return None
