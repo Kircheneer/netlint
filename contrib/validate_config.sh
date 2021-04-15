a8e73198dd65bdc30e47999c2701c9754d630757
Tonny <38716583+tonoitp@users.noreply.github.com>
2021-04-15 19:39:08 +0200

    Create validate_config.sh
    script callin netlint with changed configfile

---
+++ contrib/validate_config.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# 
+# Initate a config check
+# 
+# arguments: filename  oxidixed_modelname
+#
+
+ReportDir="$HOME/lint-reports"
+
+mkdir -p "$ReportDir"
+cfgfile=`basename $1`
+netlint -o "$ReportDir/$cfgfile.rpt" $1
